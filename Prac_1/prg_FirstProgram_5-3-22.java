de88d4814ef01c136a4cdccd8e801d6830f269a4
Snobb3ry <ARPITPATHAK.200111021@gehu.ac.in>
2022-03-05 16:39:29 +0530

    finished

---
+++ Prac_1/prg_FirstProgram_5-3-22.java
@@ -0,0 +1,23 @@
+class FirstProgram{
+    String name,uniName,brnchName;
+    int rollNo;
+
+    void set()
+        {
+            name = "Arpit Pathak";
+            uniName = "Graphic Era Hill University";
+            brnchName = "Btech - CSE";
+            rollNo = 12;
+        }
+    void show()
+        {
+            System.out.println("Name of the Student is "+name+" having roll number "+rollNo);
+            System.out.println("He is currently persuing "+brnchName+" at "+uniName);
+        }
+    public static void main(String args[])
+        {
+            FirstProgram obj = new FirstProgram();
+            obj.set();
+            obj.show();
+        }
+}
